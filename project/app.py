c4bc974e5a17eac91ad6f4910b3c61b0c8d19c36
Peter <46304666+peterjorphan@users.noreply.github.com>
2019-05-18 18:06:31 -0500

    working version without filter

--- project/app.py
+++ project/app.py
@@ -29,28 +29,28 @@ Base.prepare(db.engine, reflect=True)
 # Save references to each table
 Members_Metadata = Base.classes.members
 
-def line(a):
-    """Return the MetaData for a given sample."""
-    sel = [
-        Members_Metadata.Year,
-        Members_Metadata.SwornInAge,
-        Members_Metadata.State
-    ]
-    results = db.session.query(*sel).all()
-    df = pd.read_sql_query(results, db.session.bind)
+def line(state):
+    stmt = db.session.query(Members_Metadata).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+    
+    df_grouped = df['SwornInAge'].groupby([df['State'], df['Year']])
+    df_filtered = df_grouped
+    df_all = df['SwornInAge'].groupby(df['Year'])
 
-
-    # members_metadata = {}
-    # for result in results:
-    #     members_metadata["Year"] = result[0]
-    #     members_metadata["SwornInAge"] = result[1]
-    #     members_metadata["State"] = result[2]
-
-
-    print (df)
-    # return jsonify(members_metadata)
-
-line(1)
+    # Filter the data based on the sample number and
+    # only keep rows with values above 1
+#     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+    # Format the data to send as json
+#     data = {
+#         "Year": df.Year.values.tolist(),
+#         "SwornInAge": df.SwornInAge.values.tolist(),
+#         "State": df.State.values.tolist(),
+#     }
+    print(df_all.mean())
+    print(df_grouped.mean())
+#     print(data)
+#     return jsonify(data)
+line('MO')
 
 # @app.route("/")
 # def index():
@@ -100,22 +100,22 @@ line(1)
 #     return jsonify(sample_metadata)
 
 
-@app.route("/samples/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+# @app.route("/samples/<sample>")
+# def samples(sample):
+#     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
+#     stmt = db.session.query(Samples).statement
+#     df = pd.read_sql_query(stmt, db.session.bind)
 
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-    # Format the data to send as json
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
-    return jsonify(data)
+#     # Filter the data based on the sample number and
+#     # only keep rows with values above 1
+#     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+#     # Format the data to send as json
+#     data = {
+#         "otu_ids": sample_data.otu_id.values.tolist(),
+#         "sample_values": sample_data[sample].values.tolist(),
+#         "otu_labels": sample_data.otu_label.tolist(),
+#     }
+#     return jsonify(data)
 
 
 # if __name__ == "__main__":
