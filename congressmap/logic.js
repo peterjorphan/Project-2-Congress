b2ac25e1615d3211fa6e3fd72ba2bb3ccd45232b
ReneeOreilly <45985471+ReneeOreilly@users.noreply.github.com>
2019-05-20 22:47:57 -0500

    CongressMap
    It is working but still working on it

---
+++ congressmap/logic.js
@@ -0,0 +1,66 @@
+var AgeData = null;
+$.ajax({
+    url: "datatoreplace.json",
+    success: (data) => {
+        AgeData = data;
+    }
+  });
+
+// Creating map object
+// var map = L.map("map", {
+//   center: [38.627, -90.1994],
+//   zoom: 5
+// });
+
+// // Adding tile layer
+// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "mapbox.streets",
+//   accessToken: API_KEY
+// }).addTo(map);
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoicm9yZWlsbHkiLCJhIjoiY2p2OW1mMXg4MGF0NjN5bm1uYnBwaXB0byJ9.8KL4Alvy3N7d5HVK9Jf7Mg';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [-100.04, 38.907],
+zoom: 3
+});
+ 
+map.on('load', function () {
+console.log("test")
+map.addLayer({
+'id': 'states-layer',
+'type': 'fill',
+'source': {
+'type': 'geojson',
+'data': './states.geojson'
+},
+'paint': {
+'fill-color': 'rgba(200, 100, 240, 0.4)',
+'fill-outline-color': 'rgba(200, 100, 240, 1)'
+}
+});
+ 
+// When a click event occurs on a feature in the states layer, open a popup at the
+// location of the click, with description HTML from its properties.
+map.on('click', 'states-layer', function (e) {
+    console.log("clicked",e)
+    let stateIndex = AgeData.State.findIndex((state) => { return (state.trim().toLowerCase() === e.features[0].properties.postal.toLowerCase().replace('.', '').replace(' ', '')); });
+new mapboxgl.Popup()
+.setLngLat(e.lngLat)
+.setHTML(`${e.features[0].properties.name} ${AgeData.AverageAge[stateIndex]}`)
+.addTo(map);
+});
+ 
+// Change the cursor to a pointer when the mouse is over the states layer.
+map.on('mouseenter', 'states-layer', function () {
+map.getCanvas().style.cursor = 'pointer';
+});
+ 
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'states-layer', function () {
+map.getCanvas().style.cursor = '';
+});
+});
