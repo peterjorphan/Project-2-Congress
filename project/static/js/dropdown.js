3a6d40462a6ac6797c2a18e22b8384f9d2218b0c
Adam F <46360510+feldsteina@users.noreply.github.com>
2019-05-21 21:43:51 -0500

    working years and states dropdown menus

--- project/static/js/dropdown.js
+++ project/static/js/dropdown.js
@@ -1,11 +1,22 @@
 function init() {
     // Grab a reference to the dropdown select element
-    var selector = d3.select('#selState');
+    var selectState = d3.select('#selState');
 
     // Use the list of sample names to populate the select options
     d3.json("/states").then((stateAbbr) => {
         stateAbbr["State"].forEach((item) => {
-            selector
+            selectState
+                .append("option")
+                .text(item)
+                .property("value", item);
+        });
+    });
+
+    var selectYear = d3.select("#selYear");
+
+    d3.json("/years").then((yearJson) => {
+        yearJson["Year"].forEach((item) => {
+            selectYear
                 .append("option")
                 .text(item)
                 .property("value", item);
