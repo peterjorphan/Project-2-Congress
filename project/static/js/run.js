5196ffd6fe20c8fe6a4f5fa063dd28e366e74089
Peter <46304666+peterjorphan@users.noreply.github.com>
2019-05-22 23:24:11 -0500

    working files

--- project/static/js/run.js
+++ project/static/js/run.js
@@ -1,80 +1,87 @@
-function buildLine(state) {
+function buildLine(state = 'AK') {
     // @TODO: Use `d3.json` to fetch the sample data for the plots
     d3.json(`/line/${state}`).then((lineData) => {
-      
-      console.log(lineData)
-      // @TODO: Build a Bubble Chart using the sample data
-      d3.select("#line").html("")
+
+        console.log(lineData)
+        // @TODO: Build a Bubble Chart using the sample data
+        d3.select("#line").html("")
         var trace1 = {
-          x: lineData.Year,
-          y: lineData.AverageTotal,
-          mode: 'lines+markers',
-          type: 'scatter',
-          text: lineData.Year,
-          marker: {
-            // size: sampleData.sample_values, 
-            color: 'black'
-          },
-          name: "US" 
+            x: lineData.Year,
+            y: lineData.AverageTotal,
+            mode: 'lines+markers',
+            type: 'scatter',
+            text: lineData.Year,
+            marker: {
+                // size: sampleData.sample_values, 
+                color: 'black'
+            },
+            name: "US"
         };
 
         var trace2 = {
-          x: lineData.Year,
-          y: lineData.AverageAge,
-          mode: 'lines+markers',
-          type: 'scatter',
-          text: lineData.Year,
-          marker: {
-            // size: sampleData.sample_values, 
-            color: 'orange'
-          },
-          name: state
+            x: lineData.Year,
+            y: lineData.AverageAge,
+            mode: 'lines+markers',
+            type: 'scatter',
+            text: lineData.Year,
+            marker: {
+                // size: sampleData.sample_values, 
+                color: 'orange'
+            },
+            name: state
         };
 
         var response = [trace1, trace2];
         console.log(response)
         var layout = {
-            xaxis: {title: "Congress Year"},
-            yaxis: {title: "Average Age"},
+            xaxis: { title: "Congress Year" },
+            yaxis: { title: "Average Age" },
             width: 1000,
-            };
-  
-        Plotly.newPlot('line', response, layout, {responsive: true});
+        };
+
+        Plotly.newPlot('line', response, layout, { responsive: true });
     });
 }
 
-buildLine('MO')
+buildLine();
 
-function buildBar(year) {
+function buildBar(year = 1995) {
 
     // @TODO: Use `d3.json` to fetch the sample data for the plots
     var sampleURL = `/bar/${year}`;
-    d3.json(sampleURL).then(function(data){
+    d3.json(sampleURL).then(function (data) {
         // @TODO: Build a Bubble Chart using the sample data
         d3.select("#bar").html("")
-        var colors = {'D': 'rgb(0,0,255)',
-                'R': 'rgb(255,0,0)',
+        var colors = {
+            'D': 'rgb(0,0,255)',
+            'R': 'rgb(255,0,0)',
         }
         var barData = [{
-        x: data.State,
-        y: data.AverageAge,
-        type: "bar",
-        marker: {color: data.Party.map(p => colors[p])
-        }
+            x: data.State,
+            y: data.AverageAge,
+            type: "bar",
+
+            marker: {
+                color: data.Party.map(p => colors[p])
+            }
         }];
 
         var layout = {
-        showlegend: false,
-        xaxis: {title: "States and Territories"},
-        yaxis: {title: "Average Age"},
-        width: 1000,
+            showlegend: false,
+            xaxis: { title: "States and Territories" },
+            yaxis: { title: "Average Age" },
+            width: 1000,
+            title: {
+                display: true,
+                text: year
+            }
         };
 
-        Plotly.newPlot('bar', barData, layout, {responsive: true});
+        Plotly.newPlot('bar', barData, layout, { responsive: true });
     });
 };
 
-buildBar(1995);
+buildBar();
 
 function init() {
     // Grab a reference to the dropdown select element
